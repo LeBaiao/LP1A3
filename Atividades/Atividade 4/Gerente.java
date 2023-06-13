e922e7c74f279b19f4b1745ac060f749eda08f61
Letícia Baião <102307053+LeBaiao@users.noreply.github.com>
2023-06-13 18:35:19 -0300

    update nome atividades

--- Atividades/Atividade 4/Gerente.java
+++
@@ -1,9 +0,0 @@
-package Generics;
-
-public class Gerente extends Funcionario {
-	
-	public Gerente(String nome, int id, double salario) {
-		super(nome, id, salario);
-	}
-
-}
--- Atividades/Atividade 4/Ponto.java
+++
@@ -1,7 +0,0 @@
-package Generics;
-
-public interface Ponto {
-
-	public void horaEntrada(int horaEntrada);
-	public void horaSaida(int horaSaida);
-}
